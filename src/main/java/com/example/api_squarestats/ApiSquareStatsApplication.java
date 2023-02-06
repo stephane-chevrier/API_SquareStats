b6a370846a9a9cb88741acc37480a020b13c3dc8
stephane-chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-02-06 10:48:19 +0100

    1er commit

---
+++ src/main/java/com/example/api_squarestats/ApiSquareStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_squarestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSquareStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSquareStatsApplication.class, args);
+    }
+
+}
